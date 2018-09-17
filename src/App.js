4a2dede026e939780901df78d9482604c1527513
Piotr Kołodziejczyk <contact@frontend.london>
2018-09-17 13:22:10 +0100

    Homepage updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/reset.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Weekend Flights</h1>
-        <h2>Find the best flights for your city break</h2>
+      <div className="App">
+        <header className="header">
+          <h1>Weekend Flights</h1>
+          <h2>Find the best flights for your city break</h2>
+        </header>
       </div>
     );
   }
