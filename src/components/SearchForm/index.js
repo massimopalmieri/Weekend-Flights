a33e5703a683d69adb98644576cb3af56ba24e2a
Piotr Kołodziejczyk <contact@frontend.london>
2018-09-20 16:03:12 +0100

    sample results styling, loader added

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -63,9 +63,20 @@ class SearchForm extends Component {
     searchFlights = (e) => {
         e.preventDefault();
 
-        this.setState({
-            loading: true
-        });
+        this.setState({ loading: true });
+
+        // fetch('http://localhost:3000/results/week-40_London_06.10-08.10.json')
+        //     .then(response => response.json())
+        //     .then(json => { 
+        //         this.setState({ loading: false });
+        //         var maxAllowedPrice = 100;
+        //         var flights = _.filter(json, (flight) => {  // todo: move to backend
+        //             return flight.total_price < maxAllowedPrice
+        //         });
+
+        //         // debugger;
+        //     })
+
     }
 
     showLoader() {
@@ -89,6 +100,115 @@ class SearchForm extends Component {
                 </form>
             </div>
             <div className={this.showLoader()}></div>
+
+            <div className="flight-results">
+                <h2>From Friday 21.09.18 to Monday 24.09.18:</h2>
+                <div class="card-columns">
+                    <div class="card">
+                        <img class="card-img-top" src="images/demo/warsaw.jpg" alt="Warsaw (Poland)" />
+                        <img class="flight-flag" src="images/flags/poland.png" alt="Poland" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Warsaw, Poland
+                                <span className="float-right">£69</span>
+                            </h5>
+                            <p class="card-text">
+                                Fri 18:20 - 20:30 (2h 15m)  
+                                <span className="float-right">STN-WMI</span>
+                                <br />
+                                Mon 05:00 - 08:00 (2h 15m)  
+                                <span className="float-right">WMI-STN</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="images/demo/nice.jpg" alt="Nice (France)" />
+                        <img class="flight-flag" src="images/flags/france.png" alt="France" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Nice , France
+                                <span className="float-right">£78</span>
+                            </h5>
+                            <p class="card-text">
+                                Fri 19:10 - 21:20 (1h 45m)  
+                                <span className="float-right">LTN-NCE</span>
+                                <br />
+                                Mon 07:00 - 08:20 (1h 45m)  
+                                <span className="float-right">NCE-LTN</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="images/demo/palma.jpg" alt="Palma Mall (Spain)" />
+                        <img class="flight-flag" src="images/flags/spain.png" alt="Spain" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Palma Mall, Spain
+                                <span className="float-right">£83</span>
+                            </h5>
+                            <p class="card-text">
+                                Fri 18:40 - 20:50 (2h 10m) 
+                                <span className="float-right">STN-PMI</span> <br />
+                                Mon 05:40 - 08:10 (2h 10m) 
+                                <span className="float-right">PMI-STN</span>
+                            </p>
+                        </div>
+                    </div>
+
+
+                    <div class="card">
+                        <img class="card-img-top" src="images/demo/warsaw.jpg" alt="Warsaw (Poland)" />
+                        <img class="flight-flag" src="images/flags/poland.png" alt="Poland" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Warsaw, Poland
+                                <span className="float-right">£69</span>
+                            </h5>
+                            <p class="card-text">
+                                Fri 18:20 - 20:30 (2h 15m)  
+                                <span className="float-right">STN-WMI</span>
+                                <br />
+                                Mon 05:00 - 08:00 (2h 15m)  
+                                <span className="float-right">WMI-STN</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="images/demo/nice.jpg" alt="Nice (France)" />
+                        <img class="flight-flag" src="images/flags/france.png" alt="France" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Nice , France
+                                <span className="float-right">£78</span>
+                            </h5>
+                            <p class="card-text">
+                                Fri 19:10 - 21:20 (1h 45m)  
+                                <span className="float-right">LTN-NCE</span>
+                                <br />
+                                Mon 07:00 - 08:20 (1h 45m)  
+                                <span className="float-right">NCE-LTN</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="images/demo/palma.jpg" alt="Palma Mall (Spain)" />
+                        <img class="flight-flag" src="images/flags/spain.png" alt="Spain" />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Palma Mall, Spain
+                                <span className="float-right">£83</span>
+                            </h5>
+                            <p class="card-text">
+                                Fri 18:40 - 20:50 (2h 10m) 
+                                <span className="float-right">STN-PMI</span> <br />
+                                Mon 05:40 - 08:10 (2h 10m) 
+                                <span className="float-right">PMI-STN</span>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
           </div>
       );
     }
