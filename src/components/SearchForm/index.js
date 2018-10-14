07ea1b9e3a61d09d1fbae618aa0cd8dda3608b4c
Piotr Kołodziejczyk <contact@frontend.london>
2018-10-15 00:01:25 +0100

    max price field added

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -7,14 +7,23 @@ class SearchForm extends Component {
 
       return (  
         <div className="searchForm">
-            <form className="form-inline justify-content-center" onSubmit={handleSearchFlights}>
+            <form className="justify-content-center" onSubmit={handleSearchFlights}>
                 <div className="form-group">
-                    <label className="sr-only">From</label>
+                    <label>From</label>
                     <ResultsSelect name="airports" onChange={handleFromChange} />
                 </div>
                 <div className="form-group">
-                    <label className="sr-only">Weekend</label>
+                    <label>When</label>
                     <ResultsSelect name="weekend" onChange={handleWeekendChange} />
+                </div>
+                <div className="form-group mr-2 group-price">
+                    <label>Max price</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">£</span>
+                        </div>
+                        <input type="number" class="form-control" placeholder="100" min="10" max="10000" />
+                    </div>
                 </div>
                 <button type="submit" className="btn btn-success">Find	&raquo;</button>
             </form>
